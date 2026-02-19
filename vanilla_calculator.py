0be8a3c7e1c26d699cdd758a84420bff5af29533
Rahul <rahulkmr9211@gmail.com>
2026-02-19 12:53:28 +0530

    3. Added MOM amotization Calculation & Removed +34 in monthly EMI

--- vanilla_calculator.py
+++ vanilla_calculator.py
@@ -1,4 +1,8 @@
 from config import *
+import os
+import pandas as pd
+
+current_path = os.getcwd() + '/'
 
 mode = input('USER or AUTO: ').upper()
 if mode == 'USER':
@@ -19,7 +23,24 @@ loan_interest_rate = float(loan_interest_rate_input)/100
 #calculation Monthly EMI
 monthly_interest_rate = loan_interest_rate / 12
 total_payments_count = loan_tenure * 12
-fixed_monthly_payment = (loan_amount * monthly_interest_rate * ((1.0+monthly_interest_rate)**total_payments_count))/(((1.0+monthly_interest_rate)**total_payments_count)-1) + 34
+fixed_monthly_payment = (loan_amount * monthly_interest_rate * ((1.0+monthly_interest_rate)**total_payments_count))/(((1.0+monthly_interest_rate)**total_payments_count)-1)
 print('Your Monthly EMI:',round(fixed_monthly_payment))
+
+#Month-on-Month Amortization
+amortization_df = pd.DataFrame(columns=['MONTH','OPENING_BALANCE','EMI','INTEREST','PRINCIPAL','CLOSING_BALANCE'],index=[i for i in range(loan_tenure_input)])
+amortization_df['EMI'] = fixed_monthly_payment
+for i in range(loan_tenure_input):
+    amortization_df.loc[i,'MONTH'] = i+1
+    if i == 0:
+        amortization_df.loc[i,'OPENING_BALANCE'] = loan_amount
+    else:
+        amortization_df.loc[i,'OPENING_BALANCE'] = amortization_df.loc[i-1,'CLOSING_BALANCE']
+    amortization_df.loc[i,'INTEREST'] = amortization_df.loc[i,'OPENING_BALANCE']*monthly_interest_rate
+    amortization_df.loc[i,'PRINCIPAL'] = amortization_df.loc[i,'EMI'] - amortization_df.loc[i,'INTEREST']
+    amortization_df.loc[i,'CLOSING_BALANCE'] = amortization_df.loc[i,'OPENING_BALANCE'] - amortization_df.loc[i,'PRINCIPAL']
+
+mom_amortization_df = amortization_df.copy()
+    
+
 
 
